c55c68a1f6eca2f31b51bffdae9aabd5096fd9f4
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-06-02 20:23:54 +0300

    Added simple test

---
+++ src/commonTest/kotlin/io/baba4/syebatal/ExampleTest.kt
@@ -0,0 +1,9 @@
+package io.baba4.syebatal
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ExampleTest {
+    @Test
+    fun additionOfTwoNumbers() = assertEquals(actual = 2 + 2, expected = 4)
+}
