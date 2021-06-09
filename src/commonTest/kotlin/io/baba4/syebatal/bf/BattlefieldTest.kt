dfa5c2b8a65c6047a708cafacea507152de81637
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-06-09 11:58:12 +0300

    Added Sunk ship test

--- src/commonTest/kotlin/io/baba4/syebatal/bf/BattlefieldTest.kt
+++ src/commonTest/kotlin/io/baba4/syebatal/bf/BattlefieldTest.kt
@@ -1,18 +1,33 @@
 package io.baba4.syebatal.bf
 
 import io.baba4.syebatal.Battlefield
-import io.baba4.syebatal.BfCell
+import io.baba4.syebatal.BfCell.*
+import io.baba4.syebatal.Point
+import io.baba4.syebatal.Ship
 
 
 open class BattlefieldTest {
     protected val customBattlefield = Battlefield(size = 3) { (row, column) ->
         when (row to column) {
-            1 to 0, 1 to 2 -> BfCell.UNCHECKED
-            0 to 0, 1 to 1 -> BfCell.EMPTY
-            0 to 2, 2 to 0, 2 to 1 -> BfCell.FILLED
-            0 to 1, 2 to 2 -> BfCell.DAMAGED
+            1 to 2 -> UNCHECKED
+            0 to 0, 1 to 0 -> EMPTY
+            1 to 1,  2 to 0, 2 to 1 -> FILLED
+            0 to 1, 0 to 2, 2 to 2 -> DAMAGED
             else -> throw IllegalArgumentException("Size should be 3.")
         }
     }
-    protected val customSerialization = "3|•☒☐ • ☐☐☒"
+    protected val customSerialization = "3|•☒☒•☐ ☐☐☒"
+
+    protected val sunkShip = Ship(
+        Point(row = 0, column = 1),
+        Point(row = 0, column = 2),
+    )
+    protected val damagedShip = Ship(
+        Point(row = 2, column = 0),
+        Point(row = 2, column = 1),
+        Point(row = 2, column = 2),
+    )
+    protected val wholeShip = Ship(
+        Point(row = 1, column = 1),
+    )
 }
