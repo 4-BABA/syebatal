b77e5bc286edf8b867a59498e153be9768d377ee
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-06-15 20:27:46 +0300

    Implemented changing player after a shot

--- src/commonMain/kotlin/io/baba4/syebatal/StateModifications.kt
+++ src/commonMain/kotlin/io/baba4/syebatal/StateModifications.kt
@@ -4,8 +4,10 @@ import io.baba4.syebatal.core.elm.State
 
 
 val State.currentPlayer: Player get() = players[currentPlayerIndex]
+val State.nextPlayerIndex: Int get() = (currentPlayerIndex + 1) % players.size
 
 fun State.shoot(point: Point): State = copy(
+    currentPlayerIndex = nextPlayerIndex,
     battlefields = battlefields.mapIndexed { index, battlefield ->
         if (index == currentPlayerIndex) battlefield
         else battlefield.shotAt(point)
