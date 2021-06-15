1cb2fb2cc319f8a7f9eec12fba6093c731327584
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-06-15 20:27:23 +0300

    Added State construction checks

--- src/commonMain/kotlin/io/baba4/syebatal/core/elm/State.kt
+++ src/commonMain/kotlin/io/baba4/syebatal/core/elm/State.kt
@@ -8,4 +8,9 @@ data class State(
     val currentPlayerIndex: Int,
     val players: List<Player>,
     val battlefields: List<Battlefield1>
-) : Elm.State, Elm.ViewData
+) : Elm.State, Elm.ViewData {
+    init {
+        require(currentPlayerIndex < players.size) { "\'currentPlayerIndex\' should be in [0 .. players.size]" }
+        require(players.size == battlefields.size) { "The number of players and battlefields should be the same" }
+    }
+}
