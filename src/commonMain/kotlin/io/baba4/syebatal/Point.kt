eaca5df82d600634c2468b6e4b31ede870f72d6f
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-06-15 20:27:59 +0300

    Cleaned up Point code

--- src/commonMain/kotlin/io/baba4/syebatal/Point.kt
+++ src/commonMain/kotlin/io/baba4/syebatal/Point.kt
@@ -3,13 +3,17 @@ package io.baba4.syebatal
 
 data class Point(val row: Int, val column: Int)
 
+
 fun Point.isValid(bfSize: Int): Boolean =
     row in 0 until bfSize && column in 0 until bfSize
 
-fun Point.left(bfSize: Int): Point? = copy(column = column - 1).takeIf { it.isValid(bfSize) }
-fun Point.top(bfSize: Int): Point? = copy(row = row - 1).takeIf { it.isValid(bfSize) }
-fun Point.right(bfSize: Int): Point? = copy(column = column + 1).takeIf { it.isValid(bfSize) }
-fun Point.bottom(bfSize: Int): Point? = copy(row = row + 1).takeIf { it.isValid(bfSize) }
+fun Point.takeIfIsValid(bfSize: Int): Point? =
+    takeIf { it.isValid(bfSize) }
+
+fun Point.left(bfSize: Int): Point? = copy(column = column - 1).takeIfIsValid(bfSize)
+fun Point.top(bfSize: Int): Point? = copy(row = row - 1).takeIfIsValid(bfSize)
+fun Point.right(bfSize: Int): Point? = copy(column = column + 1).takeIfIsValid(bfSize)
+fun Point.bottom(bfSize: Int): Point? = copy(row = row + 1).takeIfIsValid(bfSize)
 
 fun Point.topLeft(bfSize: Int): Point? = top(bfSize)?.left(bfSize)
 fun Point.topRight(bfSize: Int): Point? = top(bfSize)?.right(bfSize)
