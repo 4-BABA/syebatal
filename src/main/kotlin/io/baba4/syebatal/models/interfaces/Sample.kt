8d5f9ba8c511137a5cd8cdf07dd5974895945175
andrey <osipaandr@gmail.com>
2022-05-15 16:54:44 +0400

    multiplatform -> jvm

---
+++ src/main/kotlin/io/baba4/syebatal/models/interfaces/Sample.kt
@@ -0,0 +1,11 @@
+package io.baba4.syebatal.models.interfaces
+
+import com.baba4.syebatal.models.BattleField
+import com.baba4.syebatal.models.Cell
+
+class Sample : BattleField {
+    override val cells: List<List<Cell>>
+        get() = TODO("Not yet implemented")
+    override val length: Int
+        get() = TODO("Not yet implemented")
+}
